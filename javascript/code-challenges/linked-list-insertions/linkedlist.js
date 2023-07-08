0f6091e081658316d398812b3f63f366428d3bef
saleh2001k <saleh.ayman2001@gmail.com>
2023-07-08 10:47:11 +0300

    LL zip

--- javascript/code-challenges/linked-list-insertions/linkedlist.js
+++ javascript/code-challenges/linked-list-insertions/linkedlist.js
@@ -1,48 +1,30 @@
-const Node = require('./node');
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.prev = null;
+    this.next = null;
+  }
+}
 
 class LinkedList {
   constructor() {
     this.head = null;
+    this.tail = null;
   }
 
   append(newValue) {
     const newNode = new Node(newValue);
     if (!this.head) {
       this.head = newNode;
+      this.tail = newNode;
     } else {
-      let current = this.head;
-      while (current.next) {
-        current = current.next;
-      }
-      current.next = newNode;
+      newNode.prev = this.tail;
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
   }
 
   insertBefore(value, newValue) {
-    const newNode = new Node(newValue);
-    if (!this.head) {
-      return;
-    }
-
-    if (this.head.value === value) {
-      newNode.next = this.head;
-      this.head = newNode;
-      return;
-    }
-
-    let current = this.head;
-    while (current.next) {
-      if (current.next.value === value) {
-        newNode.next = current.next;
-        current.next = newNode;
-        return;
-      }
-      current = current.next;
-    }
-  }
-
-  insertAfter(value, newValue) {
-    const newNode = new Node(newValue);
     if (!this.head) {
       return;
     }
@@ -50,7 +32,37 @@ class LinkedList {
     let current = this.head;
     while (current) {
       if (current.value === value) {
+        const newNode = new Node(newValue);
+        newNode.prev = current.prev;
+        newNode.next = current;
+        if (current.prev) {
+          current.prev.next = newNode;
+        } else {
+          this.head = newNode;
+        }
+        current.prev = newNode;
+        return;
+      }
+      current = current.next;
+    }
+  }
+
+  insertAfter(value, newValue) {
+    if (!this.head) {
+      return;
+    }
+
+    let current = this.head;
+    while (current) {
+      if (current.value === value) {
+        const newNode = new Node(newValue);
+        newNode.prev = current;
         newNode.next = current.next;
+        if (current.next) {
+          current.next.prev = newNode;
+        } else {
+          this.tail = newNode;
+        }
         current.next = newNode;
         return;
       }
@@ -59,4 +71,19 @@ class LinkedList {
   }
 }
 
-module.exports = LinkedList;
+const linkedList = new LinkedList();
+
+linkedList.append(1);
+linkedList.append(2);
+linkedList.append(3);
+
+linkedList.insertBefore(2, 5);
+
+linkedList.insertAfter(3, 6);
+
+
+let current = linkedList.head;
+while (current) {
+  console.log(current.value);
+  current = current.next;
+}
