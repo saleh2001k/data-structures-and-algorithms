5a7afbe69b2f71153593ea5e10a7b5c7d86ffb9c
saleh2001k <saleh.ayman2001@gmail.com>
2023-07-17 19:13:23 +0300

    trees

---
+++ javascript/code-challenges/Trees/BinaryTree.js
@@ -0,0 +1,42 @@
+"use strict";
+
+class BinaryTree {
+  constructor() {
+    this.root = null;
+  }
+
+  preOrder() {
+    let result = [];
+    let traverse = (node) => {
+      result.push(node.value);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    };
+    traverse(this.root);
+    return result;
+  }
+
+  inOrder() {
+    let result = [];
+    let traverse = (node) => {
+      if (node.left) traverse(node.left);
+      result.push(node.value);
+      if (node.right) traverse(node.right);
+    };
+    traverse(this.root);
+    return result;
+  }
+
+  postOrder() {
+    let result = [];
+    let traverse = (node) => {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      result.push(node.value);
+    };
+    traverse(this.root);
+    return result;
+  }
+}
+
+module.exports = BinaryTree;
